6e49b4309969a2d073b863b586761713f15353b1
Siddhant <mahatosiddhant69@gmail.com>
2023-06-15 23:26:06 +0530

    Profile-Page

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -17,6 +17,16 @@ const Feed = () => {
                     <div className='following'>
                         <h3 className='title'>Your are following</h3>
                         <Follower />
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                    </div>
+                    <div className='suggestions'>
+                        <h3 className='title'>Suggested for you</h3>
+                        <Follower />
+                        <Follower />
+                        <Follower />
+                        <Follower />
                     </div>
 
                 </div>
--- client/src/components/follower/Follower.js
+++ client/src/components/follower/Follower.js
@@ -5,8 +5,12 @@ import './Follower.scss'
 const Follower = () => {
     return (
         <div className='follower'>
-            <Avatar />
-            <h3>Shraddha</h3>
+            <div className='user-info'>
+                <Avatar />
+                <h3 className='name'>Shraddha</h3>
+            </div>
+
+            <h5 className='hover-link follow-link'>Follow</h5>
         </div>
     )
 }
